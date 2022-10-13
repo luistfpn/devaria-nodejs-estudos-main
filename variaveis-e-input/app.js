e06b1d803231b4ab4f88778efc24975679d0f88d
luistfpn <112507136+luistfpn@users.noreply.github.com>
2022-10-13 15:14:15 -0300

    node.js-estudos2

---
+++ variaveis-e-input/app.js
@@ -0,0 +1,14 @@
+const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const minhaPrimeiraConstanteString = 'Minha primeira constante';
+console.log(minhaPrimeiraConstanteString);
+
+let leituraDeCampo;
+let teste = 'teste'
+readLine.question('Informe sua idade:', t => {
+    leituraDeCampo = t;
+    console.log(`o usuario digitou: ${leituraDeCampo} e nossa variavel de teste e: ${teste}`);
+});
